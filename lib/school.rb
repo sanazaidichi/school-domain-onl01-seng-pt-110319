61552936ffb4b02eb531ac7d72c06720c20500f6
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-12-25 05:00:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School 
+  attr_accessor :name, :roster 
+  
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end 
+  
+  def add_student(name, grade)
+    if roster[grade]
+      roster[grade] << name 
+    else 
+    roster[grade] = [name]
+    end 
+  end 
+    
+    
+    
+    
+    
+    
+  end 
+  
+  
+    
